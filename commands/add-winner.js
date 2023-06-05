f03690dfe4045e0c1643356f56eab9d0a48f2628
RebeccaAnne <rebeccamuraira@live.com>
2023-06-05 12:14:23 -0700

    Date parameter

--- commands/add-winner.js
+++ commands/add-winner.js
@@ -1,5 +1,5 @@
 var fs = require("fs");
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const dayjs = require('dayjs');
 
 module.exports = {
@@ -13,14 +13,31 @@ module.exports = {
 		.addStringOption(option =>
 			option.setName('reason')
 				.setDescription('The reason for winning')
-				.setRequired(true)),
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('date')
+				.setDescription('Date won')),
 	async execute(interaction) {
 		let winner = interaction.options.getMember('winner');
 		let reason = interaction.options.getString('reason');
 		let guild = interaction.guild;
 
-		winnerFilename = "winner-arrays.json";
+		// Set the date won based on passed in parameter or today's date
+		let dateWon = dayjs(Date.now());
+		let dateWonInput = interaction.options.getString('date');
+		if (dateWonInput) {
+			dateWon = dayjs(dateWonInput);
+			if (!dateWon.isValid()) {
+				await interaction.reply({
+					embeds: [new EmbedBuilder().setDescription("Invalid Date String. Use YYYY-MM-DD format, or skip this parameter to use today's date.")],
+					ephemeral: true
+				});
+				return;
+			}
+		}
 
+		// Load the winner array from file
+		winnerFilename = "winner-arrays.json";
 		let winnerList = {}
 		try {
 			winnerList = require("..\\" + winnerFilename);
@@ -29,11 +46,10 @@ module.exports = {
 			console.log("Failed to load serverArrays from file");
 		}
 
+		// Create a winner list for this server if one doesn't already exist
 		if (winnerList[guild.id] == null) {
 			winnerList[guild.id] = [];
 		}
-
-		let replyString = "";
 
 		// Check if this user is already a winner
 		let winnerObject = {};
@@ -49,7 +65,7 @@ module.exports = {
 		// Fill in the winner object
 		winnerObject.username = winner.user.username;
 		winnerObject.id = winner.id;
-		winnerObject.date = dayjs(Date.now()).format("YYYY-MM-DD");
+		winnerObject.date = dateWon.format("YYYY-MM-DD");
 		winnerObject.reason = reason;
 
 		// Set the winner role
@@ -60,11 +76,11 @@ module.exports = {
 			winnerList[guild.id].push(winnerObject);
 		}
 
-		replyString = winner.user.username + " won the discord for " + reason + "!";
+		let replyString = winner.user.username + " won the discord for " + reason + "!";
 
 		if (winnerList[guild.id].length == 3) {
 			//Terror!!
-			replyString+=  "\nTerror of Astandalas!";
+			replyString += "\nTerror of Astandalas!";
 
 			winnerList[guild.id].forEach(async winner => {
 				let currentMember = await guild.members.fetch(winner.id)
