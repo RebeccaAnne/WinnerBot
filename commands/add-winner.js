d3ae645b202c865b3a29f346908083ea114783c6
RebeccaAnne <rebeccamuraira@live.com>
2023-06-05 17:15:18 -0700

    Terror declarations

--- commands/add-winner.js
+++ commands/add-winner.js
@@ -2,6 +2,47 @@ var fs = require("fs");
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const dayjs = require('dayjs');
 
+function getOrdinal(n) {
+	let ord = 'th';
+
+	if (n % 10 == 1 && n % 100 != 11) {
+		ord = 'st';
+	}
+	else if (n % 10 == 2 && n % 100 != 12) {
+		ord = 'nd';
+	}
+	else if (n % 10 == 3 && n % 100 != 13) {
+		ord = 'rd';
+	}
+
+	return ord;
+}
+
+async function declareTerror(guild, serverConfig, winnerList) {
+
+	terrorCount = 1;
+	if (winnerList[guild.id + "celebrationCount"]) {
+		terrorCount = winnerList[guild.id + "celebrationCount"] + 1;
+	}
+
+	let terrorString = "The " + terrorCount + getOrdinal(terrorCount) + " Terror of Astandalas! "
+
+	winnerList[guild.id].forEach(async (winner) => {
+		terrorString += "<@" + winner.id + "> ";
+
+		let currentMember = await guild.members.fetch(winner.id);
+		currentMember.roles.remove(serverConfig.winnerRoleId);
+	});
+
+	let fanworksChannel = await guild.channels.fetch(serverConfig.fanworksChannel);
+	fanworksChannel.send(terrorString);
+
+	winnerList[guild.id] = [];
+	winnerList[guild.id + "celebrationCount"] = terrorCount;
+
+	console.log(JSON.stringify(winnerList));
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('add-winner')
@@ -16,7 +57,7 @@ module.exports = {
 				.setRequired(true))
 		.addStringOption(option =>
 			option.setName('date')
-				.setDescription('Date won')),
+				.setDescription("Date won (YYYY-MM-DD), defaults to today's date")),
 	async execute(interaction) {
 		let winner = interaction.options.getMember('winner');
 		let reason = interaction.options.getString('reason');
@@ -104,16 +145,11 @@ module.exports = {
 
 		if (winnerList[guild.id].length == serverConfig.celebrationThreshold) {
 			//Terror!!
+			await declareTerror(guild, serverConfig, winnerList);
 			replyString += "\n" + serverConfig.celebrationName + "!";
-
-			winnerList[guild.id].forEach(async winner => {
-				let currentMember = await guild.members.fetch(winner.id)
-				currentMember.roles.remove(serverConfig.winnerRoleId);
-			});
-
-			winnerList[guild.id] = []
 		}
 
+		console.log(JSON.stringify(winnerList));
 		fs.writeFileSync(winnerFilename, JSON.stringify(winnerList), () => { });
 
 		// Post a congradulatory message in fanworks
